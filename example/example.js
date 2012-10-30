8089c1321a34d55fe528f32d0803c657d8651948
justan <ckr189@gmail.com>
2012-10-30 23:25:09 +0800

    works

---
+++ example/example.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+  , Canvas = require('canvas')
+  , Image = Canvas.Image
+  , tableImage = require('../').tableImage
+  , convert = tableImage.convert 
+  ;
+
+var img = new Image;
+fs.readFile('./favicon.ico', function(err, buffer){
+  img.src = buffer;
+  fs.writeFileSync('./out.html', convert(img, [255, 255, 255]), 'utf8');
+});
